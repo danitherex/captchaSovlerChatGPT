8b71639e62b93336d594fd858a0ed196974e9f22
danitherex <daniel@maurer-koblenz.de>
2024-02-06 20:16:47 +0100

    auto book an appointment

--- captcha.py
+++ captcha.py
@@ -1,35 +1,70 @@
 from openai import OpenAI
-from dotenv import load_dotenv
+import base64
+from PIL import Image
+from io import BytesIO
+import os
+import uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-load_dotenv()
+def get_captcha_code(imageUrl):
 
-client = OpenAI()
+    client = OpenAI()
+    result = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type":"text",
+                    "text":"Print out all letters or numbers visible in the right order on this image. Do not print out anything else. Just the letters and numbers. Don't include whitespaces"
+                    },
+                    {"type":"image_url",
+                    "image_url": {
+                        "url":imageUrl
+                    }
+                    }
+                ],
 
-imageUrl = input("Enter the image url: ")
+            }
+        ],
+        max_tokens=300,
+        model="gpt-4-vision-preview",
 
-result = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type":"text",
-                "text":"Print out all letters or numbers visible in the right order on this image. Do not print out anything else. Just the letters and numbers."
-                 },
-                {"type":"image_url",
-                 "image_url": {
-                     "url":imageUrl
-                 }
-                }
-            ],
+    )
 
-        }
-    ],
-    max_tokens=300,
-    model="gpt-4-vision-preview",
+    captcha_code = result.choices[0].message.content
+    return captcha_code
 
-)
 
-captcha_code = result.choices[0].message.content
+def upload_image(base64_string):
+    imageFile = convert_base64_to_image(base64_string)
     
+    connection_string = os.getenv('AZURE_CONNECTION_STRING')
+    container_name = os.getenv('AZURE_BLOB_CONTAINER_NAME')
+    
+    try:
+        # Create the BlobServiceClient that is used to call the Blob service for the storage account
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Create the BlobClient to interact with the blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=imageFile)
 
-print(captcha_code)
+        # Upload the file
+        with open(imageFile, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+            print(f"File {imageFile} uploaded to Azure Blob Storage.")
+            
+        blob_url = f"{blob_client.url}"
+        print(f"Blob URL: {blob_url}")
+        return blob_url
+        
+    except Exception as ex:
+        print('Exception:')
+        print(ex)
+    
+def convert_base64_to_image(base64_string):
+    base64_image = base64_string.split(',')[1]
+    image_data = base64.b64decode(base64_image)
+    image = Image.open(BytesIO(image_data))
+    random_file_name = f"{uuid.uuid4()}.png"
+    image.save(random_file_name)
+    return random_file_name
---
+++ termin_buchung.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from dotenv import load_dotenv
+import os
+import json
+from captcha import get_captcha_code,upload_image
+
+load_dotenv()
+
+sport_email = os.getenv("SPORT_EMAIL")
+sport_password = os.getenv("SPORT_PASSWORD")
+sport_url = os.getenv("SPORT_URL")
+_captcha = os.getenv("CAPTCHA")
+captcha = _captcha.lower() == "true"
+
+target_weekday_time = json.loads(os.getenv("WEEKDAYS"))
+
+
+def sign_up():
+
+    driver = webdriver.Chrome()
+
+    driver.get(sport_url)
+    
+    original_window = driver.current_window_handle
+    
+    
+    rows = driver.find_elements(By.CSS_SELECTOR, "tbody > tr")
+
+    row = retrieve_row(rows)
+    
+    
+    if(row == None):
+        print("No free slots available")
+        return
+    try:
+        try:
+            # Close or accept the cookie notice
+            cookie_notice_accept_button = WebDriverWait(driver,3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cookie-reject:nth-child(3)")))
+            cookie_notice_accept_button.click()
+        except Exception as e:
+            print("Cookie notice not found or could not be closed.", str(e))
+        buchenBtn = WebDriverWait(row, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".bs_btn_buchen")))
+
+        buchenBtn.click()
+        
+        WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+        windows = driver.window_handles
+        for window in windows:
+            if window != original_window:
+                driver.switch_to.window(window)
+                break
+        
+        row = driver.find_element(By.CLASS_NAME, "bs_form_row.bs_rowstripe0")
+        weekDay = row.find_element(By.CLASS_NAME,"bs_tag.alr").text
+        timeOfDate = row.find_element(By.CLASS_NAME,"bs_time").text
+        
+        standardizedTimeOfDate = timeOfDate.replace('.', ':')
+        
+        for _target_weekday, target_time in target_weekday_time.items():
+            target_weekday = _target_weekday
+            target_time = target_time
+            if(weekDay == target_weekday and standardizedTimeOfDate == target_time):
+                buchenBtn = WebDriverWait(row, 40).until(EC.element_to_be_clickable((By.CLASS_NAME, "inlbutton.buchen")))
+                buchenBtn.click()
+                
+                openPasswordField = driver.find_element(By.ID, "bs_pw_anmlink")
+                openPasswordField.click()
+                
+                emailInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#bs_pw_anm > div:nth-child(2) > div:nth-child(2) > input:nth-child(1)")))
+                paswordInput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#bs_pw_anm > div:nth-child(3) > div:nth-child(2) > input:nth-child(1)")))
+                continueButton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.bs_form_foot:nth-child(5) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)")))
+                
+                emailInput.send_keys(sport_email)
+                paswordInput.send_keys(sport_password)
+                
+                continueButton.click()
+                
+                read_checkmark = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#bs_bed > label:nth-child(1) > input:nth-child(1)")))
+                read_checkmark.click()
+                
+                continueButton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "bs_submit")))
+                continueButton.click()
+                
+                if(captcha):
+                    #Captcha handling here
+                    captcha_base64_image = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "img.width100"))).get_attribute("src")
+                    captcha_url = upload_image(captcha_base64_image)
+                    captcha_code = get_captcha_code(captcha_url)
+                    captcha_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "BS_F_captcha")))
+                    captcha_input.send_keys(captcha_code)
+                    print(captcha_code)                
+                
+                binding_booking = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.bs_right > input:nth-child(1)")))
+                binding_booking.click()
+                break      
+    except TimeoutException as e:
+        print("There seems not to be any free slots available")
+        
+    driver.quit()
+
+def retrieve_row(rows):
+    for row in rows:
+        time = row.find_element(By.CSS_SELECTOR, ".bs_szeit").text
+        weekday = row.find_element(By.CSS_SELECTOR, ".bs_stag").text
+                
+        for _target_weekday, target_time in target_weekday_time.items():
+            target_weekday = _target_weekday
+            target_time = target_time
+            if time == target_time and weekday == target_weekday:
+                return row
+        
+if __name__ == "__main__":
+    sign_up()
