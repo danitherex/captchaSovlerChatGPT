6946b8e523946f7cdc640fd6e88ef8b104b111f3
danitherex <daniel@maurer-koblenz.de>
2024-02-09 14:34:58 +0100

    Add remote debugging port option to Chrome driver

--- termin_buchung.py
+++ termin_buchung.py
@@ -35,6 +35,7 @@ def sign_up():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-dev-shm-usage");  
+    options.add_argument('--remote-debugging-port=9222')
 
     driver = webdriver.Chrome(options=options)
 
