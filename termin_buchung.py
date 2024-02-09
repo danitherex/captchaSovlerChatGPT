6310595ccc0470d7baf56d83468c5e8a68e5472a
danitherex <daniel@maurer-koblenz.de>
2024-02-09 14:32:52 +0100

    Fix ChromeOptions arguments in termin_buchung.py

--- termin_buchung.py
+++ termin_buchung.py
@@ -33,7 +33,8 @@ def sign_up():
 
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')  
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-dev-shm-usage");  
 
     driver = webdriver.Chrome(options=options)
 
