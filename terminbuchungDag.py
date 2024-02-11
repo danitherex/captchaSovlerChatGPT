75d5e6547b612651651bacc8e712c700e00ca4b5
Daniel Maurer <daniel@maurer-koblenz.de>
2024-02-11 20:44:25 +0100

    Update docker run command to use absolute path for .env file

--- terminbuchungDag.py
+++ terminbuchungDag.py
@@ -19,6 +19,6 @@ dag = DAG('Termin-Buchung', default_args=default_args, schedule_interval ="59 18
 
 t1 = BashOperator(
     task_id='book-termin',
-    bash_command='docker run --env-file .env danitherex/buchungsport',
+    bash_command='docker run --env-file /root/airflow/dags/captchaSovlerChatGPT/.env danitherex/buchungsport',
     dag=dag,
 )
